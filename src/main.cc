a541b91a8b07a5ea8dccf566c55ca18f74f9224e
Gavin Mitchell <gavator234@gmail.com>
2023-10-10 10:14:58 -0500

    Inital bits have been set up now

---
+++ src/main.cc
@@ -0,0 +1,30 @@
+/*
+    tractorlink - A program replicating the Fallout series's terminal hacking minigame.
+    Copyright (C) 2023  Gavin Mitchell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include "../include/os_headers.hh"
+
+int main(int argc, char** argv) {
+    std::cout
+    << "tractorlink  Copyright (C) 2023  Gavin Mitchell\n"
+    << "This program comes with ABSOLUTELY NO WARRANTY\n"
+    << "This is free software, and you are welcome to redistribute it\n"
+    << "under certain conditions.\n\n" << std::flush;
+    
+    return 0;
+}
